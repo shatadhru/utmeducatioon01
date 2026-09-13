ffc1d1941a33095c3b034421829da947e134ad6f
shatadhru <ssnmggs@gmail.com>
2026-09-13 20:58:55 +0600

    v3

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,72 +1,29 @@
 import type { ReactNode } from 'react'
+import React from 'react'
 
 import { AdminBar } from '@/components/AdminBar'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { LivePreviewListener } from '@/components/LivePreviewListener'
-import { ensureStartsWith } from '@/utilities/ensureStartsWith'
 import { Providers } from '@/providers'
 import { InitTheme } from '@/providers/Theme/InitTheme'
-import { GeistSans } from 'geist/font/sans'
+
 import { GeistMono } from 'geist/font/mono'
-import React from 'react'
+import { Hind_Siliguri } from 'next/font/google'
+
 import './globals.css'
 
-import localFont from 'next/font/local'
-
-const myFont = localFont({
-  src: [
-    {
-      path: '../../fonts/Li Ador Noirrit Regular.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../../fonts/Li Ador Noirrit SemiBold.ttf',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../../fonts/Li Ador Noirrit Bold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-ador-noirit-font',
+const hindSiliguri = Hind_Siliguri({
+  subsets: ['bengali'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-hind-siliguri',
 })
-
-/* const { SITE_NAME, TWITTER_CREATOR, TWITTER_SITE } = process.env
-const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : 'http://localhost:3000'
-const twitterCreator = TWITTER_CREATOR ? ensureStartsWith(TWITTER_CREATOR, '@') : undefined
-const twitterSite = TWITTER_SITE ? ensureStartsWith(TWITTER_SITE, 'https://') : undefined
- */
-/* export const metadata = {
-  metadataBase: new URL(baseUrl),
-  robots: {
-    follow: true,
-    index: true,
-  },
-  title: {
-    default: SITE_NAME,
-    template: `%s | ${SITE_NAME}`,
-  },
-  ...(twitterCreator &&
-    twitterSite && {
-      twitter: {
-        card: 'summary_large_image',
-        creator: twitterCreator,
-        site: twitterSite,
-      },
-    }),
-} */
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html
-      className={[myFont.variable, GeistMono.variable].filter(Boolean).join(' ')}
-      lang="en"
+      className={[hindSiliguri.variable, GeistMono.variable].filter(Boolean).join(' ')}
+      lang="bn"
       suppressHydrationWarning
     >
       <head>
@@ -74,6 +31,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
       </head>
+
       <body>
         <Providers>
           <AdminBar />
